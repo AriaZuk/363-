aaf264940861a5fb2c89e09ca390515683985a9f
Aria <maria.zuk@icloud.com>
2019-05-11 13:57:00 -0700

    code

--- final363SQL.sql
+++ final363SQL.sql
@@ -3,7 +3,7 @@ CREATE TABLE Film(
     title VARCHAR(255) NOT NULL,
     release_year INT,
     rating FLOAT,
-    run_time INT,
+    run_time_min INT,
     PRIMARY KEY (film_id)
 );
 CREATE TABLE Genre(
@@ -37,6 +37,7 @@ CREATE TABLE People(
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     dob DATE,
+    country_of_birth VARCHAR(255),
     PRIMARY KEY (people_id)
 );
 CREATE TABLE Role_in_film(
@@ -71,3 +72,95 @@ CREATE TABLE HasEpisode (
     ep_id INT,
     PRIMARY KEY (season_id, ep_id)
 );
+
+INSERT INTO Film
+VALUES
+(1, 'The Ides of March', 2011, 7.1, 101),
+(2, 'Thor: Ragnarok', 2017, 7.9, 130),
+(3, 'What We Do in the Shadows', 2014, 7.7, 86),
+(4, 'Avengers: Endgame', 2019, 7.9, 181);
+
+INSERT INTO TvSeries
+VALUES
+(1, 'Community', 22, 2009, 2015, 8.5);
+
+INSERT INTO People
+VALUES
+( 1, 'George', 'Clooney', '1961-05-06', 'USA'),
+(2, 'Taika', 'Waititi', '1975-08-16', 'New Zealand'),
+(3, 'Anthony', 'Russo', '1970-02-03', 'USA'),
+(4, 'Joe', 'Russo', '1971-07-08', 'USA');
+
+INSERT INTO Role_in_film
+VALUES
+( 1, 1, 'Actor'),
+( 1, 1, 'Director'),
+( 1, 1, 'Writer'),
+( 1, 1, 'Producer'),
+( 2, 2, 'Director'),
+( 2, 2, 'Actor'),
+( 3, 2, 'Director'),
+( 3, 2, 'Actor'),
+( 3, 2, 'Writer'),
+( 3, 2, 'Producer'),
+( 4, 3, 'Director'),
+( 4, 4, 'Director');
+
+INSERT INTO Role_in_tv
+VALUES
+( 1, 3, 'Producer'),
+( 1, 3, 'Director'),
+( 1, 4, 'Producer'),
+( 1, 4, 'Director');
+
+INSERT INTO Film_gen
+VALUES
+(1, 'Drama'),
+(1, 'Thriller'),
+(2, 'Action'),
+(2, 'Adventure'),
+(2, 'Comedy'),
+(3, 'Comedy'),
+(3, 'Horror');
+
+INSERT INTO Tv_gen
+VALUES
+(1, 'Comedy');
+
+INSERT INTO Season
+VALUES
+(1, 1, 25),
+(2, 2, 24),
+(3, 3, 22),
+(4, 4, 13),
+(5, 5, 13),
+(6, 6, 13);
+
+INSERT INTO HasSeason
+VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6);
+
+INSERT INTO Episode
+VALUES
+(1, 1, 'Pilot', '2009-09-17'),
+(2, 2, 'Spanish 101', '2009-09-24'),
+(3, 3, 'Intorduction to Film', '2009-10-01'),
+(4, 4, 'Social Psychology', '2009-10-08'),
+(5, 5, 'Advanced Criminal Law', '2009-10-15'),
+(6, 6, 'Football, Feminism and You', '2009-10-22');
+
+INSERT INTO HasEpisode
+VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6);
+
+select first_name, last_name, title from people natural join Role_in_film natural join film where roleIn = 'Actor';
